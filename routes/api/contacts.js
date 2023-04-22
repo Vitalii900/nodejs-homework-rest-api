625af30c9db4ab00f29a36bbce49266258742c16
Vitalii Melnyk <melnikvitaliy30@gmail.com>
2023-04-22 14:02:00 +0300

    start work

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,49 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const contactsOperations = require('../../models/contacts');
+
+const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const contacts = await contactsOperations.listContacts();
+    res.json(contacts);
+  } catch (error) {
+    res.status(500).json({
+      message: 'Server error'
+    });
+  }
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { contactId } = req.params;
+    const result = await contactsOperations.getContactById(contactId);
+    if (!result) {
+      res.status(404).json({
+        message: `Contact with id=${contactId} not found`,
+      });
+      return;
+    }
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({
+      message: 'Server error',
+    });
+  }
+
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
-module.exports = router
+module.exports = router;
