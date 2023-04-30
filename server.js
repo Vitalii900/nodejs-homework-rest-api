95a15c413ebda6219240c4679dfaba7a08d73a06
Vitalii Melnyk <melnikvitaliy30@gmail.com>
2023-04-30 14:34:51 +0300

    server update

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const app = require('./app')
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  'mongodb+srv://Vitalii:PTgi6bB89pDbeAaa@cluster0.mw6mnan.mongodb.net/contacts_reader?retryWrites=true&w=majority';
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000)
+    console.log('Database connection successful')
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
