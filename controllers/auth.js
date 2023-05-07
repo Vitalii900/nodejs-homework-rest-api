1e3d1b2bfe51563d9b193d5dcc2351b434179008
Vitalii Melnyk <melnikvitaliy30@gmail.com>
2023-05-07 21:08:13 +0300

    create login and register routers

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,15 @@
 const { User } = require('../models/user');
 const { HttpError, ctrlWrapper } = require('../helpers');
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user) {
-    throw HttpError(409, "Email already in use");
+    throw HttpError(409, 'Email already in use');
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
@@ -17,7 +19,7 @@ const register = async (req, res) => {
     email: newUser.email,
     password: newUser.password,
   });
-}
+};
 
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -30,7 +32,17 @@ const login = async (req, res) => {
   if (!passwordCompare) {
     throw HttpError(401, 'Email or password is wrong');
   }
-}
+
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+  res.json({
+    token,
+  });
+};
 
 module.exports = {
   register: ctrlWrapper(register),
